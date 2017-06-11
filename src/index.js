9f501f40438328e541b7bc01aa68107fe57a9773
jundy.chow <zhoujundi711@163.com>
2017-06-11 15:10:05 +0800

    引入mobx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react'
+import ReactDOM from 'react-dom';
 import { Provider } from "mobx-react";
 import stores from './store'
 
